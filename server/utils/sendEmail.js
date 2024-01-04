50b028f19ed3588b78886fac437c312f086abc60
Garrett <garrettswink421@gmail.com>
2024-01-03 23:16:36 -0500

    Commit changes

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,18 +1,14 @@
 const nodemailer = require("nodemailer");
 
-// Change variable name to send message.
-// Adjust parameters. Likely remove reply_to, but maybe also sent_from
-// Remember to make argument + function adjustments to match the parameters
-// You will need to adjust the post req in the app.js file too
 const sendEmail = async (subject, message, send_to, sent_from) => {
 
 // Create the transporter
   const transporter = nodemailer.createTransport({
     // Update the host when deploying?
     host: process.env.EMAIL_HOST,
-    // Port also likely needs to be updated. Using Gmail for the short term.
+    // Port also likely needs to be updated.
     port: "587",
-    // See Nodemailer documentation. I believe this will be the gmail credentials. Eventually need to add withAuth
+    
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
@@ -23,8 +19,6 @@ const sendEmail = async (subject, message, send_to, sent_from) => {
     },
   });
 
-//   If you adjust the parameters in sendEmail, these properties will also need to be updated.
-//  Also, the post req in App.js
   const options = {
     from: sent_from,
     to: send_to,
