cb03d928e058874661774e9a187d102609840d28
Garrett <garrettswink421@gmail.com>
2024-01-19 20:06:50 -0500

    experiment

--- server/app.js
+++ server/app.js
@@ -39,10 +39,10 @@ app.post("/api/sendemail", async (req, res) => {
   }
 });
 
-app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use(express.static(path.join(__dirname, '../../client/dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
 });
 
 const PORT = process.env.PORT || 3001;
